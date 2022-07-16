d0f6dba83694e00a9cfe1fe0f50df81af2ed198d
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2022-07-16 02:48:32 -0400

    Added documentation to each object created

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,12 @@
 from tkinter import *
 
+# Creates window
 root = Tk()
 
+# Creates label widget printing hello world
 label = Label(root, text="Hello World")
 
+# Places label onto window
 label.pack()
 
 root.mainloop()
