d9a1282b35808383a937aebb212dc22d4fdd3ac2
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2022-07-16 03:25:31 -0400

    Added function to be called when button is clicked

--- buttons.py
+++ buttons.py
@@ -3,8 +3,13 @@ from tkinter import *
 # Builds main window
 root = Tk()
 
+# Adds label object below current packed object on screen
+def myClick():
+    label = Label(root, text="I clicked this button!")
+    label.pack()
+
 # Creates button on screen saying click me
-button = Button(root, text="Click Me!", padx=50, pady=50)
+button = Button(root, text="Click Me!", padx=50, pady=50, command=myClick)
 
 # Places button on window
 button.pack()
