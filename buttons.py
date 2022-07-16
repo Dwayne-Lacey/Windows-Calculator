a364c8fda692313e6c3b9bb23098c59efbb75045
Dwayne Lacey <dwaynelacey.acm@gmail.com>
2022-07-16 03:19:16 -0400

    Created button with click me message and placed it in window

--- buttons.py
+++ buttons.py
@@ -2,3 +2,11 @@ from tkinter import *
 
 # Builds main window
 root = Tk()
+
+# Creates button on screen saying click me
+button = Button(root, text="Click Me!", padx=50, pady=50)
+
+# Places button on window
+button.pack()
+
+root.mainloop()
